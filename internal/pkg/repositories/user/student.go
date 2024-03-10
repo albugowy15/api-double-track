49d65f43f94305c4d9f5214d4b7a2013a3e20d4c
albugowy15 <kholidbughowi@gmail.com>
2024-03-11 05:50:11 +0700

    feat: add and update student repository

--- internal/pkg/repositories/user/student.go
+++ internal/pkg/repositories/user/student.go
@@ -1,6 +1,8 @@
 package user
 
 import (
+	"log"
+
 	"github.con/albugowy15/api-double-track/internal/pkg/db"
 	"github.con/albugowy15/api-double-track/internal/pkg/models/user"
 )
@@ -38,4 +40,33 @@ func (r *StudentRepository) GetStudentBySchoolId(schoolId string, studentId stri
 	student := user.Student{}
 	err := db.GetDb().Get(&student, "SELECT id, username, email, phone_number, fullname, nisn FROM students WHERE school_id = $1 AND id = $2", schoolId, studentId)
 	return student, err
+}
+
+func (r *StudentRepository) AddStudent(schoolId string, data user.Student) error {
+	_, err := db.GetDb().Exec("INSERT INTO students (username, password, fullname, nisn, school_id) VALUES ($1, $2, $3, $4, $5)", data.Username, data.Password, data.Fullname, data.Nisn, schoolId)
+	return err
+}
+
+func (r *StudentRepository) DeleteStudent(studentId string) error {
+	_, err := db.GetDb().Exec("DELETE FROM students WHERE id = $1", studentId)
+	if err != nil {
+		log.Println(err)
+	}
+	return err
+}
+
+func (r *StudentRepository) UpdateStudent(studentId string, data user.Student) error {
+	_, err := db.GetDb().Exec(
+		"UPDATE students SET fullname = $1, username = $2, nisn = $3, email = $4, phone_number = $5 WHERE id = $6",
+		data.Fullname,
+		data.Username,
+		data.Nisn,
+		data.Email,
+		data.PhoneNumber,
+		studentId,
+	)
+	if err != nil {
+		log.Println(err)
+	}
+	return err
 }

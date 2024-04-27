f6fa34f3603ed3aecda4d08326df7a24b20695b2
albugowy15 <kholidbughowi@gmail.com>
2024-04-27 11:21:28 +0700

    fix: get recommendations by school query

--- internal/repositories/recommendations.go
+++ internal/repositories/recommendations.go
@@ -8,16 +8,16 @@ import (
 )
 
 func GetRecommendationsBySchoolId(schoolId string) ([]models.StudentRecommendation, error) {
-	students := []models.StudentRecommendation{}
+	var studentsRecs []models.StudentRecommendation
 	tx, err := db.AppDB.Beginx()
 	if err != nil {
 		log.Println("db err:", err)
-		return students, err
+		return studentsRecs, err
 	}
 
 	err = tx.Select(
-		&students,
-		`SELECT ahp.id, ahp.consistency_ratio, s.id as student_id, s.fullname, s.nisn FROM ahp 
+		&studentsRecs,
+		`SELECT ahp.consistency_ratio, s.id as student_id, s.fullname, s.nisn FROM ahp 
     INNER JOIN students s ON s.id = ahp.student_id 
     WHERE s.school_id = $1`,
 		schoolId,
@@ -25,34 +25,36 @@ func GetRecommendationsBySchoolId(schoolId string) ([]models.StudentRecommendati
 	if err != nil {
 		log.Println("db err:", err)
 		tx.Rollback()
-		return students, err
+		return studentsRecs, err
 	}
 
-	for idx, student := range students {
+	for idx, rec := range studentsRecs {
 		ahpResults := []models.RecommendationResult{}
 		err = tx.Select(
 			&ahpResults,
 			`SELECT ata.id, ata.score, a.alternative, a.description FROM ahp_to_alternatives ata 
       INNER JOIN alternatives a ON a.id = ata.alternative_id 
-      WHERE ahp_id = $1
+      INNER JOIN ahp ON ahp.id = ata.ahp_id
+      INNER JOIN students s ON s.id = ahp.student_id
+      WHERE s.id = $1
       ORDER BY ata.score DESC`,
-			student.Id,
+			rec.StudentId,
 		)
 		if err != nil {
 			log.Println("db err:", err)
 			tx.Rollback()
-			return students, err
+			return studentsRecs, err
 		}
-		students[idx].AhpResults = ahpResults
+		studentsRecs[idx].AhpResults = ahpResults
 
 		// do query for topsis
 	}
 
-	return students, nil
+	return studentsRecs, nil
 }
 
 func GetAHPRecommendations(studentId string) ([]models.RecommendationResult, error) {
-	recommendations := []models.RecommendationResult{}
+	var recommendations []models.RecommendationResult
 	err := db.AppDB.Select(
 		&recommendations,
 		`SELECT ata.id, a.alternative, ata.score, a.description FROM ahp_to_alternatives ata 

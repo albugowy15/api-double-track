ffceeee092a5cdaf49a02e7de2fa180f8bdf1608
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-07-10 12:58:21 +0700

    fix: typo in alternatives description

--- db/seeds/alternatives.go
+++ db/seeds/alternatives.go
@@ -16,7 +16,7 @@ func SeedAlternativeTx(tx *sqlx.Tx) {
 	alteratives := []Alternative{
 		{Alternative: "Multimedia", Description: "Alternative keterampilan Multimedia"},
 		{Alternative: "Teknik Elektro", Description: "Alternative keterampilan Teknik Elektro"},
-		{Alternative: "Teknik Listrik", Description: "Alternative keterampilan Tata Listrik"},
+		{Alternative: "Teknik Listrik", Description: "Alternative keterampilan Teknik Listrik"},
 		{Alternative: "Tata Busana", Description: "Alternative keterampilan Tata Busana"},
 		{Alternative: "Tata Boga", Description: "Alternative keterampilan Tata Boga"},
 		{Alternative: "Tata Kecantikan", Description: "Alternative keterampilan Tata Kecantikan"},
